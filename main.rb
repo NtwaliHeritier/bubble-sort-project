59f810e7f1aff72fec404b246a4e5dfea88d168a
enkog <nkirukaogu@gmail.com>
2020-09-10 16:42:11 +0100

    Add the bubble_sort_by method

--- main.rb
+++ main.rb
@@ -13,4 +13,22 @@ def bubble_sort(arr)
     arr
 end
 
-puts bubble_sort([2,1,5,6,8,7])
+def bubble_sort_by(arr)
+    return arr if arr.length<=1
+    swap = true
+    while swap do
+        swap=false
+        (arr.length-1).times do |x|
+            if yield(arr[x], arr[x+1]) >= 0
+                arr[x], arr[x+1]=arr[x+1], arr[x]
+                swap=true
+            end
+        end
+    end
+    p arr
+end
+
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+    left.length - right.length
+end
